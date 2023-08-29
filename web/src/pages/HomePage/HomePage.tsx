50c2497fa6194e89290e036d1d35c4fb5584c356
Tawnee <allprt89@gmail.com>
2023-08-29 10:52:59 -0600

    make events selectable

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -4,63 +4,10 @@ import { useEffect, useMemo, useState } from 'react'
 import { useGeolocated } from 'react-geolocated'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-const testData = [
-  {
-    lat: 42.877742,
-    long: -107.448006,
-    id: 'Location1',
-  },
-  {
-    lat: 44.020882,
-    long: -107.955234,
-    id: 'Location2',
-  },
-  {
-    lat: 43.754787,
-    long: -110.736289,
-    id: 'Location3',
-  },
-  {
-    lat: 41.314755,
-    long: -105.587806,
-    id: 'Location4',
-  },
-  {
-    lat: 42.850797,
-    long: -106.324661,
-    id: 'Location5',
-  },
-  {
-    lat: 43.075967,
-    long: -107.290283,
-    id: 'Location6',
-  },
-  {
-    lat: 42.8666,
-    long: -109.864731,
-    id: 'Location7',
-  },
-  {
-    lat: 44.798626,
-    long: -106.961725,
-    id: 'Location8',
-  },
-  {
-    lat: 42.866793,
-    long: -109.881067,
-    id: 'Location9',
-  },
-  {
-    lat: 43.173587,
-    long: -110.946226,
-    id: 'Location10',
-  },
-]
-
 const HomePage = () => {
   const { isAuthenticated, logIn, logOut, userMetadata } = useAuth()
 
-  const [currentEvent, setCurrentEvent] = useState<string>(null)
+  const [currentEventId, setCurrentEventId] = useState<string>(null)
   const [viewState, setViewState] = useState({
     latitude: 37.8,
     longitude: -122.4,
@@ -74,7 +21,10 @@ const HomePage = () => {
           latitude={lat}
           longitude={long}
           key={id}
-          onClick={() => setCurrentEvent(id)}
+          onClick={() => {
+            setViewState({ latitude: lat, longitude: long, zoom: 15 })
+            setCurrentEventId(id)
+          }}
         >
           <span className="text-3xl">🎸</span>
         </Marker>
@@ -82,6 +32,7 @@ const HomePage = () => {
     [testData]
   )
 
+  // The user's current location
   const { coords } = useGeolocated({
     positionOptions: {
       enableHighAccuracy: false,
@@ -100,22 +51,45 @@ const HomePage = () => {
   }, [coords])
 
   return (
-    <div>
-      {isAuthenticated ? (
-        <div>
-          <p>{userMetadata?.username}</p>
-          <img src={userMetadata?.imageUrl} alt="" className="w-12" />
-          <button onClick={() => logOut()}>Log Out</button>
-        </div>
-      ) : (
-        <button onClick={() => logIn()}>Log In</button>
-      )}
+    <div className="relative">
+      <div className="fixed left-0 top-0 z-20 w-full bg-gray-600 text-white">
+        {isAuthenticated ? (
+          <div>
+            <p>{userMetadata?.username}</p>
+            <img src={userMetadata?.imageUrl} alt="" className="w-12" />
+            <button onClick={() => logOut()}>Log Out</button>
+          </div>
+        ) : (
+          <button onClick={() => logIn()}>Log In</button>
+        )}
+
+        {/* Shows list of events */}
+        {testData.map(({ id, lat, long }) => (
+          <button
+            onClick={() => {
+              setViewState({ latitude: lat, longitude: long, zoom: 15 })
+              setCurrentEventId(id)
+            }}
+          >
+            {id}
+          </button>
+        ))}
+
+        {/* shows selected event info */}
+        {currentEventId && (
+          <div className="bg-black">
+            <p>{testData.find(({ id }) => id === currentEventId).id}</p>
+            <p>{testData.find(({ id }) => id === currentEventId).title}</p>
+          </div>
+        )}
+      </div>
 
       <Map
         {...viewState}
         reuseMaps
         onMove={(e) => setViewState(e.viewState)}
-        onClick={(e) => console.log(e)}
+        // Close current event info when user drags map
+        onDrag={() => setCurrentEventId(null)}
         mapLib={import('mapbox-gl')}
         style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}
         mapStyle="mapbox://styles/tawnee-ik/cllv4qnri006601r6dx3t2hqh"
@@ -136,3 +110,66 @@ const HomePage = () => {
 }
 
 export default HomePage
+
+const testData = [
+  {
+    lat: 42.877742,
+    long: -107.448006,
+    id: 'Location1',
+    title: 'Title of Event 1',
+  },
+  {
+    lat: 44.020882,
+    long: -107.955234,
+    id: 'Location2',
+    title: 'Title of Event 2',
+  },
+  {
+    lat: 43.754787,
+    long: -110.736289,
+    id: 'Location3',
+    title: 'Title of Event 3',
+  },
+  {
+    lat: 41.314755,
+    long: -105.587806,
+    id: 'Location4',
+    title: 'Title of Event 4',
+  },
+  {
+    lat: 42.850797,
+    long: -106.324661,
+    id: 'Location5',
+    title: 'Title of Event 5',
+  },
+  {
+    lat: 43.075967,
+    long: -107.290283,
+    id: 'Location6',
+    title: 'Title of Event 6',
+  },
+  {
+    lat: 42.8666,
+    long: -109.864731,
+    id: 'Location7',
+    title: 'Title of Event 7',
+  },
+  {
+    lat: 44.798626,
+    long: -106.961725,
+    id: 'Location8',
+    title: 'Title of Event 8',
+  },
+  {
+    lat: 42.866793,
+    long: -109.881067,
+    id: 'Location9',
+    title: 'Title of Event 9',
+  },
+  {
+    lat: 43.173587,
+    long: -110.946226,
+    id: 'Location10',
+    title: 'Title of Event 10',
+  },
+]
