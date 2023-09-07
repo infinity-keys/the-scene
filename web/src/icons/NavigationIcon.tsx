e0a35a8a8426e083aea76e362b20a5d9ebae8426
IvySwing <entense@att.net>
2023-09-07 18:58:47 +0000

    added icons

---
+++ web/src/icons/NavigationIcon.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+const NavigationIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.998}
+      d="m12 2 7 19-7-4-7 4 7-19Z"
+    />
+  </svg>
+)
+export default NavigationIcon
---
+++ web/src/icons/RefreshIcon.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+const RefreshIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.998}
+      d="M.999 4v6h6M23 20v-6h-6"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.998}
+      d="M20.489 9a9 9 0 0 0-14.85-3.36L.999 10m22 4-4.64 4.36A8.999 8.999 0 0 1 3.509 15"
+    />
+  </svg>
+)
+export default RefreshIcon
