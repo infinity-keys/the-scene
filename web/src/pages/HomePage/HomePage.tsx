8e28c60f1e0c82905aafc4cf9fe129f75b08815c
Tawnee <allprt89@gmail.com>
2023-09-09 09:05:13 -0600

    update logos and layout

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -7,14 +7,13 @@ const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home Page" />
-      <div className="mx-auto flex min-h-screen w-full max-w-md flex-col justify-center gap-6 p-4">
-        <p className="text-center text-2xl font-medium text-white">what's up</p>
+      <div className="lg:lp-0 mx-auto flex min-h-screen w-full max-w-md flex-col gap-6 p-4 pt-24 lg:justify-center">
         <div className="grid h-full max-h-[472px] w-full flex-1 grid-cols-1 gap-3">
           <Link
-            className="relative block h-full w-full rounded bg-[linear-gradient(to_bottom_right,rgba(0,0,0,0.3),rgba(0,0,0,0)),url('/images/ShareImage.webp')] bg-cover text-lg text-white shadow hover:shadow-white"
+            className="relative block h-full w-full rounded bg-[linear-gradient(to_bottom_right,rgba(0,0,0,0.5),rgba(0,0,0,0)),url('/images/ShareImage.webp')] bg-cover text-lg text-white shadow hover:shadow-white"
             to={routes.share()}
           >
-            <span className="absolute left-8 top-8 block w-full max-w-[80px] ">
+            <span className="absolute left-8 top-8 block w-full max-w-[100px] ">
               <ShareTitle />
             </span>
           </Link>
@@ -23,7 +22,7 @@ const HomePage = () => {
             className="relative block h-full w-full rounded bg-[linear-gradient(to_bottom_right,rgba(0,0,0,0),rgba(0,0,0,.3)),url('/images/FindImage.webp')] bg-cover text-lg text-white shadow hover:shadow-white"
             to={routes.find()}
           >
-            <span className="absolute bottom-8 right-8 block w-full max-w-[70px]">
+            <span className="absolute bottom-8 right-8 block w-full max-w-[100px]">
               <FindTitle />
             </span>
           </Link>
