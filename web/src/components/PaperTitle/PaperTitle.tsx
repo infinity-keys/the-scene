7776df5cea87f2f1cce150a3b8045c363c4334f8
Tawnee <allprt89@gmail.com>
2023-09-15 13:02:31 -0600

    add type in animation to paper title

--- web/src/components/PaperTitle/PaperTitle.tsx
+++ web/src/components/PaperTitle/PaperTitle.tsx
@@ -1,10 +1,26 @@
-import { PropsWithChildren } from 'react'
+import { TypeAnimation } from 'react-type-animation'
 
-const PaperTitle = ({ children }: PropsWithChildren) => {
+const PaperTitle = ({
+  text,
+  withAnimation = false,
+}: {
+  text: string
+  withAnimation?: boolean
+}) => {
   return (
     <div className="-rotate-2">
-      <p className="paper inline bg-[url('/images/paper.webp')] bg-cover bg-no-repeat px-2 py-1 text-2xl font-black uppercase leading-[2.8rem] text-black">
-        {children}
+      <p className="paper inline bg-[url('/images/paper.webp')] bg-cover bg-no-repeat px-2 py-1 text-3xl font-black uppercase leading-[2.8rem] text-black">
+        {withAnimation ? (
+          <TypeAnimation
+            sequence={[200, text]}
+            speed={55}
+            cursor={false}
+            wrapper="span"
+            repeat={1}
+          />
+        ) : (
+          text
+        )}
       </p>
     </div>
   )
